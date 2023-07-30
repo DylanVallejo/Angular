ba11401d76bd8dc26a37752ff5259a6cb8f82d48
DylanVallejo <dylan1811vallejo@gmail.com>
2023-07-29 22:57:20 -0500

    Creando un contador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+  title = 'Mi primer app en Angular';
+  public counter:number = 1;
+  increaseBy(value : number):void{
+    this.counter += value;
+  }
+  decreaseBy(value : number):void{
+    if(this.counter > 1){
+      console.log(value)
+      this.counter -= value;
+
+    }else{
+      alert('No puedes restar mas de uno')
+    }
+  }
+  reset():void{
+    this.counter = 1
+  }
 }
