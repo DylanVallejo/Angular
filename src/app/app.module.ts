57ced0f8c33492115de2e30eef5d69f3b8d71cff
DylanVallejo <dylan1811vallejo@gmail.com>
2023-08-03 00:00:06 -0500

    estructura de modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
+// import { CounterComponent } from './counter/counter.component';
+import { HeroComponent } from './heroes/components/hero/hero.component';
+import { ListComponent } from './heroes/components/list/list.component';
+import { CounterModule } from './counter/components/counter.module';
+import { HeroModule } from './heroes/hero.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent,
-    HeroComponent,
-    ListComponent,
+    // CounterComponent,
+    // HeroComponent,
+    // ListComponent,
 
   ],
+  // cuando se usa modulos va en imports
   imports: [
-    BrowserModule
+    BrowserModule,
+    CounterModule,
+    HeroModule
   ],
   providers: [],
   bootstrap: [AppComponent]
