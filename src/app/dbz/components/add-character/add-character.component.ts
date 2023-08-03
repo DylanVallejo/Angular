e9eca33df9e3e40772f9286f7ef7f155f7903d71
DylanVallejo <dylan1811vallejo@gmail.com>
2023-08-03 17:26:58 -0500

    emitir eventos al componente padre desde el hijo con @Output()

--- src/app/dbz/components/add-character/add-character.component.ts
+++ src/app/dbz/components/add-character/add-character.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Character } from '../../interfaces/character.interface';
+
 
 @Component({
   selector: 'app-dbz-add-character',
@@ -8,6 +9,9 @@ import { Character } from '../../interfaces/character.interface';
 })
 
 export class AddCharacterComponent {
+  @Output()
+  public onNewCharacter:EventEmitter<Character> = new EventEmitter();
+
   public character: Character = {
     name: '',
     power: 0
@@ -15,5 +19,12 @@ export class AddCharacterComponent {
 
   emitCharacter():void{
     console.log(this.character);
+    if(this.character.name.length === 0 ) return;
+
+    this.onNewCharacter.emit(this.character);
+    this.character.name="";
+    this.character.power=0;
+
   }
+
 }
