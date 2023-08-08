0d35dc9f854dcd5fee2ce93b0370f31413c7c86d
DylanVallejo <dylan1811vallejo@gmail.com>
2023-08-08 00:21:21 -0500

    volver un servicio privado y refactorizr el codigo para usarlo

--- src/app/dbz/services/dbz.service.ts
+++ src/app/dbz/services/dbz.service.ts
@@ -36,7 +36,7 @@ export class DbzService {
     }
   ];
 
-  onNewCharacter(character: Character ):void{
+  addCharacter(character: Character ):void{
     const newCharacter: Character = {
       id: uuid(),
       ...character
